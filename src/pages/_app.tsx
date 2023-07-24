d5fde3743e1f9a85d4924576b0068bd384f43d8c
notray.hackshaw@gmail.com <36725950+Ray-Hackshaw@users.noreply.github.com>
2023-07-24 22:44:30 +1200

    feat: fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,25 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import localFont from "next/font/local";
+
+const Wix = localFont({
+  src: [
+    {
+      path: "../../public/fonts/WixMadeforText-Medium.ttf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-wix",
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${Wix.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default api.withTRPC(MyApp);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+            wix: ["var(--font-wix)"]
+        },
+      colors: {
+        blue: "#14213D",
+        sun: "#FCA311",
+        cloud: "#E5E5E5",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
