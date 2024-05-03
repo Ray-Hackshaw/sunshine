9090dacdc6780ac0325b1247cf4c0f4073266089
Ray-Hackshaw <ray.hackshaw@gmail.com>
2024-05-04 00:42:55 +1200

    refactor: bit of rain

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,40 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import { Layout } from "~/components/Layout";
-import { Map } from "~/components/Map";
-import { api } from "~/utils/api";
-import { calculateIsohels } from "~/utils/calculateIsohels";
-import type { Pairing } from "~/utils/interfaces";
-import { Loading } from "~/components/Loading";
-import { capitalize } from "~/server/utils/textFormat";
-import Link from "next/link";
+// import { useEffect, useState } from "react";
+// import { Layout } from "~/components/Layout";
+// import { Map } from "~/components/Map";
+// import { api } from "~/utils/api";
+// import { calculateIsohels } from "~/utils/calculateIsohels";
+// import type { Pairing } from "~/utils/interfaces";
+// import { Loading } from "~/components/Loading";
+// import { capitalize } from "~/server/utils/textFormat";
+// import Link from "next/link";
 
 const HomePage: NextPage = () => {
-  const { data: sunlights, isLoading } = api.isohel.getAllData.useQuery(
-    undefined,
-    {
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-    }
-  );
+  //   const { data: sunlights, isLoading } = api.isohel.getAllData.useQuery(
+  //     undefined,
+  //     {
+  //       refetchOnWindowFocus: false,
+  //       refetchOnMount: false,
+  //     }
+  //   );
 
-  const [points, setPoints] = useState<Pairing[]>();
+  //   const [points, setPoints] = useState<Pairing[]>();
 
-  useEffect(() => {
-    if (points) return;
-    if (sunlights) {
-      const isohels = calculateIsohels({ isohels: sunlights });
-      setPoints(isohels);
-    }
-  }, [points, sunlights]);
+  //   useEffect(() => {
+  //     if (points) return;
+  //     if (sunlights) {
+  //       const isohels = calculateIsohels({ isohels: sunlights });
+  //       setPoints(isohels);
+  //     }
+  //   }, [points, sunlights]);
 
-  const uniqueCities = [
-    ...new Set([
-      ...(points?.map((x) => capitalize(x.firstCity)) ?? []),
-      ...(points?.map((x) => capitalize(x.secondCity)) ?? []),
-    ]),
-  ].join(", ");
+  //   const uniqueCities = [
+  //     ...new Set([
+  //       ...(points?.map((x) => capitalize(x.firstCity)) ?? []),
+  //       ...(points?.map((x) => capitalize(x.secondCity)) ?? []),
+  //     ]),
+  //   ].join(", ");
 
   return (
     <>
@@ -46,15 +46,34 @@ const HomePage: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {!points && (
+      <div className="flex min-h-screen w-full items-center justify-center bg-dark">
+        <div className=" font-wix text-white">
+          <p className="py-2 text-center text-xl">
+            Oops, looks like its raining.
+          </p>
+          <div className="space-y-2">
+            <p>
+              This app was originally hosted with a Planetscale database on
+              their legacy free tier plan.
+            </p>
+            <p>That free plan no longer exists.</p>
+            <p>
+              For now, this site will be inactive until I migrate to a different
+              provider. Apologies.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* {!points && (
         <div className="flex min-h-screen w-full items-center justify-center bg-dark">
           <div className="flex flex-col items-center gap-3">
             <p className="font-wix text-white">Loading...</p>
             <Loading />
           </div>
         </div>
-      )}
-      {points && !isLoading && (
+      )} */}
+      {/* {points && !isLoading && (
         <Layout>
           <div className="mx-auto flex w-full flex-col gap-8 px-4 py-6 font-wix md:min-h-screen md:px-8 md:py-8">
             <Map points={points} />
@@ -82,7 +101,7 @@ const HomePage: NextPage = () => {
             <Link href="/calculations">next</Link>
           </div>
         </Layout>
-      )}
+      )} */}
     </>
   );
 };
